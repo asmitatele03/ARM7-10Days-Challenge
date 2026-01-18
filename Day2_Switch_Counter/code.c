5e6f388888ae39cfc4cc60cde855cb0b8c0278fa
Asmita Tele <asmitatele03@gmail.com>
2026-01-18 12:18:45 +0530

    Update code.c

--- Day2_Switch_Counter/code.c
+++ Day2_Switch_Counter/code.c
@@ -1,1 +1,21 @@
+#include <LPC21xx.H>
+#include "header.h"
+
+int main() {
+    int c = 0;
+    IODIR0 = 1;
+
+    while(1) {
+        if(((IOPIN0 >> 5) & 1) == 0) {
+            IOCLR0 = 1;
+            while(((IOPIN0 >> 5) & 1) == 0);
+            c++;
+        }
+        else {
+            IOSET0 = 1;
+            if(c == 10)
+                c = 0;
+        }
+    }
+}
 
