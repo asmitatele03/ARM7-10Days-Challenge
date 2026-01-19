7a6daadc1e896857fb2d504277f1e960d3d9abb9
Asmita Tele <asmitatele03@gmail.com>
2026-01-19 15:48:59 +0530

    Update code.c

--- Day3_LED_Switch/code.c
+++ Day3_LED_Switch/code.c
@@ -1,1 +1,18 @@
+// two way switch
+#include <LPC21xx.H>
+
+int main() {
+    IODIR0 = 1 << 17;       // Configure P0.17 as output (LED)
+    IODIR1 = 0x00;          // Configure Port1 pins as input (Switches)
+
+    while(1) {
+        // Check if P1.14 or P1.15 is HIGH (switch pressed)
+        if (((IOPIN1 >> 14) & 1) || ((IOPIN1 >> 15) & 1)) {
+            IOCLR0 = 1 << 17;   // Turn LED ON
+        }
+        else {
+            IOSET0 = 1 << 17;   // Turn LED OFF
+        }
+    }
+}
 
