41346970615255c7a7d6d570e979ca12828ad5a8
Asmita Tele <asmitatele03@gmail.com>
2026-01-20 17:04:34 +0530

    Update code.c

--- Day4_Switch_LED/code.c
+++ Day4_Switch_LED/code.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    IODIR0 = 1 << 1;     // Configure P0.1 as output
+    IODIR0 = 1;     // Configure P0.1 as output
 
     while (1)
     {
         if (((IOPIN0 >> 5) & 1) == 0)   // Read input at P0.5 (active low)
         {
-            IOCLR0 = 1 << 1;           // Clear P0.1 → LED ON
+            IOCLR0 = 1;           // Clear P0.1 → LED ON
         }
         else
         {
-            IOSET0 = 1 << 1;           // Set P0.1 → LED OFF
+            IOSET0 = 1;           // Set P0.1 → LED OFF
         }
     }
 }
