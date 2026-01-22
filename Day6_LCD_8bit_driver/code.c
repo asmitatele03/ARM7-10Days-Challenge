5a7278610e98c875cc3b5866f85e3eb65c836662
Asmita Tele <asmitatele03@gmail.com>
2026-01-22 15:02:02 +0530

    Update code.c

--- Day6_LCD_8bit_driver/code.c
+++ Day6_LCD_8bit_driver/code.c
@@ -32,6 +32,40 @@ extern void delay_sec(unsigned int sec);
 extern void delay_ms(unsigned int ms);
 
 lcd_8bit_driver.c
+#include <LPC21xx.h>
+#include "header.h"
+
+void lcd_data(unsigned char data)
+{
+    IOCLR0 = 0x7F;         // Clear data pins
+    IOSET0 = data;         // Put data on port
+    IOSET0 = 1<<8;         // RS = 1 (data)
+    IOCLR0 = 1<<9;         // RW = 0 (write)
+    IOSET0 = 1<<10;        // EN = 1
+    delay_ms(2);
+    IOCLR0 = 1<<10;        // EN = 0
+}
+
+void lcd_cmd(unsigned char cmd)
+{
+    IOCLR0 = 0x7F;         // Clear data pins
+    IOSET0 = cmd;          // Command output
+    IOCLR0 = 1<<8;         // RS = 0 (command)
+    IOCLR0 = 1<<9;         // RW = 0
+    IOSET0 = 1<<10;        // EN = 1
+    delay_ms(2);
+    IOCLR0 = 1<<10;        // EN = 0
+}
+
+void lcd_init()
+{
+    IODIR0 = 0x7FF;        // Set P0.0–P0.10 as output
+    IOCLR0 = 0x7FF;
+
+    lcd_cmd(0x38);         // 8-bit, 2 line
+    lcd_cmd(0x0E);         // Display ON, cursor ON
+    lcd_cmd(0x01);         // Clear display
+}
 
 
 
