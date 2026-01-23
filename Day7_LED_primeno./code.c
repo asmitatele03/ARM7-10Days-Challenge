122059bde90ba5c452438979e6941f1f4fd6fd32
Asmita Tele <asmitatele03@gmail.com>
2026-01-23 15:09:45 +0530

    Add prime number detection in LED display
    Implement prime number detection using LEDs.

--- Day7_LED_primeno./code.c
+++ Day7_LED_primeno./code.c
@@ -1,1 +1,25 @@
+#include <LPC21xx.H>
+#include "header.h"
+
+int main() {
+    int i = 1, j, c;
+    IODIR0 = 255;
+    IOSET0 = 255;
+
+    while(1) {
+
+        for (j = 1, c = 0; j <= i; j++) {
+            if (i % j == 0)
+                c++;
+        }
+
+        if (c == 2) {
+            IOCLR0 = 1 << i;
+            delay_ms(200);
+            IOSET0 = 1 << i;
+        }
+
+        i++;
+    }
+}
 
