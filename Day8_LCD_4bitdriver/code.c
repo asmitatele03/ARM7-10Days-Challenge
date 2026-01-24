e015f75d2cf2a87eaff4c42a5d612595ffff19c4
Asmita Tele <asmitatele03@gmail.com>
2026-01-24 15:32:10 +0530

    Remove return statement and delay function
    Removed return statement and delay_ms function.

--- Day8_LCD_4bitdriver/code.c
+++ Day8_LCD_4bitdriver/code.c
@@ -16,13 +16,6 @@ int main()
             lcd_cmd(0x01);       // Clear display
         }
     }
-    return 0;
 }
 
-void delay_ms(unsigned int ms)
-{
-    unsigned int i, j;
-    for(i = 0; i < ms; i++)
-        for(j = 0; j < 6000; j++);    // Approx 1ms delay for 60MHz CCLK
-}
 
