2ba957ddf511c0bae728a381abace363bc488820
Asmita Tele <asmitatele03@gmail.com>
2026-01-24 15:42:05 +0530

    Fix formatting of clear display command

--- Day8_LCD_4bitdriver/code.c
+++ Day8_LCD_4bitdriver/code.c
@@ -13,7 +13,7 @@ int main()
             lcd_cmd(0x80 + i);   // Move cursor to position i
             lcd_data('A');       // Display character 'A'
             delay_ms(500);       // Delay to show movement
-            lcd_cmd(0x01);       // Clear display
+            lcd_cmd(0x01);       // Clear display 
         }
     }
 }
