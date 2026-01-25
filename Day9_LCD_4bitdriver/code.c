46b9b4b277799da4d3b53ca9e6a47599866395a4
Asmita Tele <asmitatele03@gmail.com>
2026-01-25 14:20:00 +0530

    Add main function for LCD character display
    Implement a main function to initialize LCD and display characters.

--- Day9_LCD_4bitdriver/code.c
+++ Day9_LCD_4bitdriver/code.c
@@ -1,1 +1,19 @@
+main()
+{
+    int i;
+    lcd_init();
+    while(1)
+    {
+        for(i = 0; i < 16; i++)
+        {
+            lcd_cmd(0x80 + i);   // First row, moving forward
+            lcd_data('A');
 
+            lcd_cmd(0xC0 + (15 - i)); // Second row, moving backward
+            lcd_data('Z');
+
+            delay_ms(500);
+            lcd_cmd(0x01);     // Clear display
+        }
+    }
+}
