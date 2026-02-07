7a63a97966eabc1c2fafbb0ea10eae9b2580b8bb
Asmita Tele <asmitatele03@gmail.com>
2026-02-07 18:51:00 +0530

    Update code.c

--- Day3_LED_Switch/code.c
+++ Day3_LED_Switch/code.c
@@ -7,7 +7,8 @@ int main() {
 
     while(1) {
         // Check if P1.14 or P1.15 is HIGH (switch pressed)
-        if (((IOPIN1 >> 14) & 1) || ((IOPIN1 >> 15) & 1)) {
+        if (((IOPIN1 >> 14) & 1) || ((IOPIN1 >> 15) & 1)) 
+        {
             IOCLR0 = 1 << 17;   // Turn LED ON
         }
         else {
